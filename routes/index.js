e333312b2b7556a941af48aca840be95b7325b53
WayneHan <1830261482@qq.com>
2016-09-01 15:10:38 +0800

    intiversion

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto'),
-  User = require('../models/user.js');
+  User = require('../models/user.js'),
+  Post = require('../models/post.js');;
 var express = require('express');
 var router = express.Router();
 
@@ -10,12 +11,32 @@ var router = express.Router();
 
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    res.render('index', { title: '主页' });
+    Post.get(null, function (err, posts) {
+      if (err) {
+        posts = [];
+      }
+      res.render('index', {
+        title: '主页',
+        user: req.session.user,
+        posts: posts,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
   });
 
+
+  app.get('/reg', checkNotLogin);
   app.get('/reg', function (req, res) {
-    res.render('reg', { title: 'Registor' });
+    res.render('reg', {
+      title: 'Registor',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
   });
+
+  app.post('/reg', checkNotLogin);
   app.post('/reg', function (req, res) {
     var name = req.body.name,
       password = req.body.password,
@@ -25,6 +46,7 @@ module.exports = function (app) {
       req.flash('error', '两次输入的密码不一致!');
       return res.redirect('/reg');//返回注册页
     }
+    console.log(req.body);
     //生成密码的 md5 值
     var md5 = crypto.createHash('md5'),
       password = md5.update(req.body.password).digest('hex');
@@ -56,19 +78,88 @@ module.exports = function (app) {
     });
   });
 
+
+  app.get('/login', checkNotLogin);
   app.get('/login', function (req, res) {
-    res.render('login', { title: 'Login' });
+    res.render('login', {
+      title: '登录',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
   });
+
+  app.post('/login', checkNotLogin);
   app.post('/login', function (req, res) {
+    //生成密码的 md5 值
+    var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+    //检查用户是否存在
+    User.get(req.body.name, function (err, user) {
+      if (!user) {
+        req.flash('error', '用户不存在!');
+        return res.redirect('/login');//用户不存在则跳转到登录页
+      }
+      //检查密码是否一致
+      if (user.password != password) {
+        req.flash('error', '密码错误!');
+        return res.redirect('/login');//密码错误则跳转到登录页
+      }
+      //用户名密码都匹配后，将用户信息存入 session
+      req.session.user = user;
+      req.flash('success', '登陆成功!');
+      res.redirect('/');//登陆成功后跳转到主页
+    });
   });
 
+
+  app.get('/post', checkLogin);
   app.get('/post', function (req, res) {
-    res.render('post', { title: 'Post' });
-  });
-  app.post('/post', function (req, res) {
+    res.render('post', {
+      title: 'Post',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
   });
 
-  app.get('/logout', function (req, res) {
-    res.render('logout', { title: 'Logout' });
+  app.post('/post', checkLogin);
+  app.post('/post', checkLogin);
+  app.post('/post', function (req, res) {
+    var currentUser = req.session.user,
+      post = new Post(currentUser.name, req.body.title, req.body.post);
+    post.save(function (err) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      req.flash('success', '发布成功!');
+      res.redirect('/');//发表成功跳转到主页
+    });
   });
+
+
+  app.get('/logout', checkLogin);
+  app.get('/logout', function (req, res) {
+    req.session.user = null;
+    req.flash('success', '登出成功!');
+    res.redirect('/');//登出成功后跳转到主页
+  });
+
+
+  function checkLogin(req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录!');
+      res.redirect('/login');
+    }
+    next();
+  }
+
+  function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录!');
+      res.redirect('back');
+    }
+    next();
+  }
 };
